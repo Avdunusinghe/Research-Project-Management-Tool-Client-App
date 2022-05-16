de31b24adea90f1a73a2367de3b8c081a9acca07
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-16 13:28:21 +0530

    Config Web Pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -6,11 +9,9 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   resolve: {
-    alias: {
-      components: path.resolve(__dirname, "src/components"),
-    },
     extensions: [".js", ".jsx"],
   },
   module: {
@@ -21,14 +22,59 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
+        enforce: "pre",
+        use: ["source-map-loader"],
       },
+
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+      },
+      { test: /\.txt$/, use: "raw-loader" },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        exclude: /node_modules/,
+        loader: "file-loader",
+        options: {
+          limit: 1024,
+          name: "[name].[ext]",
+          publicPath: "dist/assets/",
+          outputPath: "dist/assets/",
+        },
+      },
     ],
   },
+
   devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
     port: 3000,
+    historyApiFallback: true,
+    hot: true,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.config().parsed),
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
 };
