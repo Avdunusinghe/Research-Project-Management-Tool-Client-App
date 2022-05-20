906b940912ca32c30a91bf35cde06505df72b574
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-20 23:43:22 +0530

    Bug dev

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 const SideBar = () => {
   return (
     <div className="sidebar">
       <div className="top">
         <span className="logo">AF</span>
       </div>
-      <div className="center">list</div>
+      <hr></hr>
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Users</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
       <div className="bottom">mode</div>
     </div>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <SideBar />
+      <div className="homeContainer">container</div>
     </div>
   );
 };
