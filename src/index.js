135416749085b55721e909080e52febfbb5bf5b7
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-22 11:15:15 +0530

    Change BreakPonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 import "./App.scss";
-
+import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
--- src/pages/authentication/login.jsx
+++ src/pages/authentication/login.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import "./login.scss";
-
 const Login = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
   return <div></div>;
 };
 
