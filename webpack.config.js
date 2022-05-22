75d907a4b27273eb5dfb3cee473e84489d8cc372
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-22 16:41:46 +0530

    Add React Toast Service

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,56 @@ const dotenv = require("dotenv");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  target: "web",
-  resolve: {
-    extensions: [".js", ".jsx", "json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js",
+		publicPath: "/",
+	},
+	target: "web",
+	resolve: {
+		extensions: [".js", ".jsx", "json"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
 
-        //enforce: "pre",
-      },
+				//enforce: "pre",
+			},
 
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
 
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-  },
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "public"),
+		},
+		compress: true,
+		port: 3000,
+		historyApiFallback: true,
+		hot: true,
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(dotenv.config().parsed),
-    }),
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({ template: "./public/index.html" }),
+		new webpack.DefinePlugin({
+			"process.env": JSON.stringify(dotenv.config().parsed),
+		}),
+		new webpack.ProvidePlugin({
+			React: "react",
+		}),
+	],
 };
