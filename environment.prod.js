d517e2886c96708c6b0700414fde192351a41b4e
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-24 19:25:58 +0530

    Add Prod URL

---
+++ environment.prod.js
@@ -0,0 +1,6 @@
+const environment = {
+	apiUrl: "http://localhost:4000/api",
+	//prodApiUrl: "https://rpmt-backend-api.herokuapp.com/",
+};
+
+export default environment;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import axios from "axios";
 
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.css";
+
+const currentUser = JSON.parse(localStorage.getItem("currentUer"));
+axios.defaults.baseURL = "http://localhost:4000";
+//axios.defaults.headers.common["RPMT-token"] = currentUser.token;
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
