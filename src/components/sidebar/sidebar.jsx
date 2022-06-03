e46db1ee1512b56ec76c7effdd41683ffcec77e5
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-03 18:33:48 +0530

    access control created

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -71,7 +71,7 @@ const SideBar = () => {
 						</Link>
 					</li>
 
-					{currentUser.isAdmin === true && (
+					{currentUser.isPanelMember === true && (
 						<li>
 							<Link to="/evaluationlist" style={{ textDecoration: "none"}}>
 								<TopicIcon className="icon" />
